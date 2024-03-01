60362d10d5a3565e5ca41c7bca053ff0ce8bb842
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-03-01 14:17:28 +0100

    Fix include sisci_error.h path

--- lib/error_util.h
+++ lib/error_util.h
@@ -1,7 +1,7 @@
 #ifndef KRITJOLIB_SISCI_ERROR_UTILITY
 #define KRITJOLIB_SISCI_ERROR_UTILITY
 
-#include "/opt/DIS/include/sisci_error.h"
+#include "sisci_error.h"
 
 void print_sisci_error(sci_error_t *error, char *func, int exit_on_failure);
 
