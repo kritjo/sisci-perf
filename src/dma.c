03d9eb0383201b59b9065f7c7510d4ac515afd79
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-03-06 14:35:48 +0100

    Refactor dma sending to smaller functions

--- src/dma.c
+++ src/dma.c
@@ -14,6 +14,46 @@
 
 #define SEND_SEG_ID 4589
 
+void dma_send_test(sci_desc_t v_dev, sci_remote_segment_t remote_segment, bool use_sysdma, unsigned int channel_id) {
+    sci_local_segment_t local_segment;
+    rdma_buff_t* local_map_address;
+    sci_map_t local_map;
+    sci_map_t remote_map = NULL;
+    sci_error_t error;
+    sci_dma_queue_t dma_queue;
+    sci_dma_channel_t dma_channel = NULL;
+
+    dma_init(v_dev, remote_segment, &dma_queue, remote_map);
+
+    SCICreateSegment(v_dev, &local_segment, SEND_SEG_ID, sizeof(rdma_buff_t), NO_CALLBACK, NO_ARG, SCI_FLAG_PRIVATE, &error);
+    print_sisci_error(&error, "SCICreateSegment", true);
+
+    SCIPrepareSegment(local_segment, ADAPTER_NO, NO_FLAGS, &error);
+
+    local_map_address = (rdma_buff_t*)SCIMapLocalSegment(local_segment, &local_map, NO_OFFSET, sizeof(rdma_buff_t), NO_SUG_ADDR, NO_FLAGS, &error);
+    print_sisci_error(&error, "SCIMapLocalSegment", true);
+
+    local_map_address->done = 0;
+    strcpy(local_map_address->word, "OK");
+
+    send_dma_buff(&dma_queue, local_segment, remote_segment, use_sysdma);
+
+    local_map_address->done = 1;
+    send_dma_buff(&dma_queue, local_segment, remote_segment, use_sysdma);
+
+    if (channel_id != 1) dma_channel_init(v_dev, local_segment, remote_segment, channel_id, &dma_queue, dma_channel);
+
+    send_dma_buff(&dma_queue, local_segment, remote_segment, use_sysdma);
+
+    local_map_address->done = 1;
+    send_dma_buff(&dma_queue, local_segment, remote_segment, use_sysdma);
+
+    SCIRemoveSegment(local_segment, NO_FLAGS, &error);
+
+    if (channel_id != 1) dma_channel_destroy(dma_channel, local_segment, remote_segment);
+    dma_destroy(local_map, remote_map, dma_queue);
+}
+
 
 void send_dma_buff(sci_dma_queue_t *dma_queue, sci_local_segment_t local_segment, sci_remote_segment_t remote_segment, bool use_sysdma) {
     sci_dma_queue_state_t dma_queue_state;
@@ -45,71 +85,27 @@ void send_dma_buff(sci_dma_queue_t *dma_queue, sci_local_segment_t local_segment
     }
 }
 
-void dma(sci_desc_t v_dev, sci_remote_segment_t remote_segment, bool use_sysdma, unsigned int channel_id) {
-    sci_dma_queue_t dma_queue;
+void dma_init(sci_desc_t v_dev, sci_remote_segment_t remote_segment, sci_dma_queue_t *dma_queue, sci_map_t remote_map) {
     sci_error_t error;
     sci_dma_queue_state_t dma_q_state;
-    sci_dma_channel_t dma_channel;
-    sci_local_segment_t local_segment;
-    rdma_buff_t* local_map_address;
-    sci_map_t local_map;
-    sci_map_t remote_map;
-
-    SCICreateSegment(v_dev, &local_segment, SEND_SEG_ID, sizeof(rdma_buff_t), NO_CALLBACK, NO_ARG, SCI_FLAG_PRIVATE, &error);
-    print_sisci_error(&error, "SCICreateSegment", true);
-
-    SCIPrepareSegment(local_segment, ADAPTER_NO, NO_FLAGS, &error);
-
-    local_map_address = (rdma_buff_t*)SCIMapLocalSegment(local_segment, &local_map, NO_OFFSET, sizeof(rdma_buff_t), NO_SUG_ADDR, NO_FLAGS, &error);
-    print_sisci_error(&error, "SCIMapLocalSegment", true);
-
-    local_map_address->done = 0;
-    strcpy(local_map_address->word, "OK");
 
     SCIMapRemoteSegment(remote_segment, &remote_map, NO_OFFSET, sizeof(rdma_buff_t), NO_SUG_ADDR, NO_FLAGS, &error);
     print_sisci_error(&error, "SCIMapRemoteSegment", true);
 
-    SCICreateDMAQueue(v_dev, &dma_queue, ADAPTER_NO, 1, NO_FLAGS, &error);
+    SCICreateDMAQueue(v_dev, dma_queue, ADAPTER_NO, 1, NO_FLAGS, &error);
     print_sisci_error(&error, "SCICreateDMAQueue", true);
 
-    if (channel_id != -1) {
-        SCIRequestDMAChannel(v_dev, &dma_channel, ADAPTER_NO, SCI_DMA_TYPE_SYSTEM, channel_id, NO_FLAGS, &error);
-        print_sisci_error(&error, "SCIRequestDMAChannel", true);
-
-        SCIAssignDMAChannel(dma_channel, dma_queue, NO_FLAGS, &error);
-        print_sisci_error(&error, "SCIAssignDMAChannel", true);
-
-        SCIPrepareLocalSegmentForDMA(dma_channel, local_segment, NO_FLAGS, &error);
-        print_sisci_error(&error, "SCIPrepareLocalSegmentForDMA", true);
-
-        SCIPrepareRemoteSegmentForDMA(dma_channel, remote_segment, NO_FLAGS, &error);
-        print_sisci_error(&error, "SCIPrepareRemoteSegmentForDMA", true);
-    }
-
-    dma_q_state = SCIDMAQueueState(dma_queue);
+    dma_q_state = SCIDMAQueueState(*dma_queue);
     if (dma_q_state == SCI_DMAQUEUE_IDLE)
         printf("DMA queue is idle\n");
     else {
         fprintf(stderr, "DMA queue is not idle\n");
         exit(EXIT_FAILURE);
     }
+}
 
-    send_dma_buff(&dma_queue, local_segment, remote_segment, use_sysdma);
-
-    local_map_address->done = 1;
-    send_dma_buff(&dma_queue, local_segment, remote_segment, use_sysdma);
-
-    if (channel_id != -1) {
-        SCIUnprepareRemoteSegmentForDMA(dma_channel, remote_segment, NO_FLAGS, &error);
-        print_sisci_error(&error, "SCIUnprepareRemoteSegmentForDMA", true);
-
-        SCIUnprepareLocalSegmentForDMA(dma_channel, local_segment, NO_FLAGS, &error);
-        print_sisci_error(&error, "SCIUnprepareLocalSegmentForDMA", true);
-
-        SCIReturnDMAChannel(dma_channel, &error);
-        print_sisci_error(&error, "SCIReturnDMAChannel", true);
-    }
-
+void dma_destroy(sci_map_t local_map, sci_map_t remote_map, sci_dma_queue_t dma_queue) {
+    sci_error_t error;
     SCIRemoveDMAQueue(dma_queue, NO_FLAGS, &error);
     print_sisci_error(&error, "SCIRemoveDMAQueue", false);
 
@@ -118,4 +114,33 @@ void dma(sci_desc_t v_dev, sci_remote_segment_t remote_segment, bool use_sysdma,
 
     SCIUnmapSegment(remote_map, NO_FLAGS, &error);
     print_sisci_error(&error, "SCIUnmapSegment", false);
+}
+
+void dma_channel_destroy(sci_dma_channel_t dma_channel, sci_local_segment_t local_segment, sci_remote_segment_t remote_segment) {
+    sci_error_t error;
+
+    SCIUnprepareRemoteSegmentForDMA(dma_channel, remote_segment, NO_FLAGS, &error);
+    print_sisci_error(&error, "SCIUnprepareRemoteSegmentForDMA", true);
+
+    SCIUnprepareLocalSegmentForDMA(dma_channel, local_segment, NO_FLAGS, &error);
+    print_sisci_error(&error, "SCIUnprepareLocalSegmentForDMA", true);
+
+    SCIReturnDMAChannel(dma_channel, &error);
+    print_sisci_error(&error, "SCIReturnDMAChannel", true);
+}
+
+void dma_channel_init(sci_desc_t v_dev, sci_local_segment_t local_segment, sci_remote_segment_t remote_segment, unsigned int channel_id, sci_dma_queue_t *dma_queue, sci_dma_channel_t dma_channel) {
+    sci_error_t error;
+
+    SCIRequestDMAChannel(v_dev, &dma_channel, ADAPTER_NO, SCI_DMA_TYPE_SYSTEM, channel_id, NO_FLAGS, &error);
+    print_sisci_error(&error, "SCIRequestDMAChannel", true);
+
+    SCIAssignDMAChannel(dma_channel, *dma_queue, NO_FLAGS, &error);
+    print_sisci_error(&error, "SCIAssignDMAChannel", true);
+
+    SCIPrepareLocalSegmentForDMA(dma_channel, local_segment, NO_FLAGS, &error);
+    print_sisci_error(&error, "SCIPrepareLocalSegmentForDMA", true);
+
+    SCIPrepareRemoteSegmentForDMA(dma_channel, remote_segment, NO_FLAGS, &error);
+    print_sisci_error(&error, "SCIPrepareRemoteSegmentForDMA", true);
 }
--- src/dma.h
+++ src/dma.h
@@ -6,7 +6,11 @@
 #include "sisci_types.h"
 #include "rdma_buff.h"
 
+void dma_send_test(sci_desc_t v_dev, sci_remote_segment_t remote_segment, bool use_sysdma, unsigned int channel_id);
 void send_dma_buff(sci_dma_queue_t *dma_queue, sci_local_segment_t local_segment, sci_remote_segment_t remote_segment, bool use_sysdma);
-void dma(sci_desc_t v_dev, sci_remote_segment_t remote_segment, bool use_sysdma, unsigned int channel_id);
+void dma_init(sci_desc_t v_dev, sci_remote_segment_t remote_segment, sci_dma_queue_t *dma_queue, sci_map_t remote_map);
+void dma_destroy(sci_map_t local_map, sci_map_t remote_map, sci_dma_queue_t dma_queue);
+void dma_channel_destroy(sci_dma_channel_t dma_channel, sci_local_segment_t local_segment, sci_remote_segment_t remote_segment);
+void dma_channel_init(sci_desc_t v_dev, sci_local_segment_t local_segment, sci_remote_segment_t remote_segment, unsigned int channel_id, sci_dma_queue_t *dma_queue, sci_dma_channel_t dma_channel);
 
 #endif //KRITJO_SISCI_DMA
