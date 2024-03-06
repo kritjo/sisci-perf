d169f77dc03bd8bf386031ce0b48a51a9792b3ea
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-03-06 11:50:06 +0100

    Generalise arg parsing a bit

--- src/rnid_util.c
+++ src/rnid_util.c
@@ -11,8 +11,17 @@
 #include "error_util.h"
 #include "sisci_glob_defs.h"
 
+#define ARG_INIT(num_ptr) \
+            *num_ptr = -1;
 
-bool parse_uint(char *arg, unsigned int *receiver_node_id) {
+#define ARG_PARSE(num_ptr, arg, argc, argv, parse_func, print_usage) \
+            if (*num_ptr != -1) print_usage(argv[0]); \
+            if (arg+1 == argc) print_usage(argv[0]); \
+            parse_func(argv[arg + 1], num_ptr); \
+            if (*num_ptr == -1) print_usage(argv[0]);
+
+
+static void parse_uint(char *arg, unsigned int *receiver_node_id) {
     long strtol_tmp;
     char *endptr;
 
@@ -24,14 +33,13 @@ bool parse_uint(char *arg, unsigned int *receiver_node_id) {
         exit(EXIT_FAILURE);
     }
 
-    if (strtol_tmp > UINT_MAX || strtol_tmp < 0) return false;
-    if (*endptr != '\0') return false;
+    if (strtol_tmp > UINT_MAX || strtol_tmp < 0) return;
+    if (*endptr != '\0') return;
 
     *receiver_node_id = (unsigned int) strtol_tmp;
-    return true;
 }
 
-bool parse_an(char *arg, unsigned int *receiver_node_id) {
+static void parse_an(char *arg, unsigned int *receiver_node_id) {
     dis_virt_adapter_t adapter_type;
     sci_error_t error;
     dis_nodeId_list_t nodelist;
@@ -44,36 +52,26 @@ bool parse_an(char *arg, unsigned int *receiver_node_id) {
         *receiver_node_id = nodelist[0];
     } else {
         printf("No matching adapters found matching %s\n", arg);
-        return false;
+        return;
     }
 
     if (nodelist[1] != 0) printf("    (multiple adapters found)\n");
-    return true;
 }
 
-int parse_id_args(int argc, char *argv[], unsigned int *id, bool *is_channel, void (*print_usage)(char *)) {
-    bool opt_set = false;
+int parse_id_args(int argc, char *argv[], unsigned int *rnid, unsigned int *channel_id, void (*print_usage)(char *)) {
+    ARG_INIT(rnid)
+    ARG_INIT(channel_id)
     int arg;
 
     for (arg = 0; arg < argc; arg++) {
         if (strcmp(argv[arg], "-nid") == 0) {
-            if (opt_set) print_usage(argv[0]);
-            if (arg+1 == argc) print_usage(argv[0]);
-            opt_set = parse_uint(argv[arg + 1], id);
-            if (!opt_set) print_usage(argv[0]);
+            ARG_PARSE(rnid, arg, argc, argv, parse_uint, print_usage)
         }
         else if (strcmp(argv[arg], "-an") == 0) {
-            if (opt_set) print_usage(argv[0]);
-            if (arg+1 == argc) print_usage(argv[0]);
-            opt_set = parse_an(argv[arg + 1], id);
-            if (!opt_set) print_usage(argv[0]);
+            ARG_PARSE(rnid, arg, argc, argv, parse_an, print_usage)
         }
         else if (strcmp(argv[arg], "-chid") == 0) {
-            if (opt_set) print_usage(argv[0]);
-            if (arg+1 == argc) print_usage(argv[0]);
-            opt_set = parse_uint(argv[arg + 1], id);
-            if (!opt_set) print_usage(argv[0]);
-            *is_channel = true;
+            ARG_PARSE(channel_id, arg, argc, argv, parse_uint, print_usage)
         }
     }
     return arg;
