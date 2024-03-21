383e7891a44858faf140abddb3d5c44f45ede8a4
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-03-21 20:21:53 +0100

    rma check

--- src/receiver.c
+++ src/receiver.c
@@ -13,12 +13,12 @@
 #include "sisci_glob_defs.h"
 #include "args_parser.h"
 #include "common.h"
+#include "lib_rma.h"
 
 void print_usage(char *prog_name) {
     printf("usage: %s [-nid <opt> | -an <opt>] [-chid <opt>] <mode>\n", prog_name);
-    printf("    -nid <sender node id>         : Specify the sender using node id\n");         // required later
-    printf("    -an <sender adapter name>     : Specify the sender using its adapter name\n");// required later
-    //printf("    -chid <channel id>            : Specify the DMA channel id, required for mode dma-channel\n");
+    printf("    -nid <sender node id>         : Specify the sender using node id\n");
+    printf("    -an <sender adapter name>     : Specify the sender using its adapter name\n");
     printf("    <mode>                        : Mode of operation\n");
     printf("           poll                   : Busy wait for transfer\n");
     printf("           rma                    : Map remote segment, and read from it directly\n");
@@ -26,7 +26,7 @@ void print_usage(char *prog_name) {
     exit(EXIT_FAILURE);
 }
 
-static void poll(sci_desc_t v_dev, unsigned int local_node_id, unsigned int receiver_id, unsigned int channel_id) {
+static void poll(sci_desc_t v_dev, unsigned int local_node_id) {
     sci_error_t error;
     sci_local_segment_t local_segment;
     sci_map_t local_map;
@@ -57,30 +57,25 @@ static void poll(sci_desc_t v_dev, unsigned int local_node_id, unsigned int rece
 }
 
 static void rma(sci_desc_t v_dev, unsigned int receiver_id) {
-    sci_error_t error = SCI_ERR_OK;
     sci_map_t remote_map;
-    rdma_buff_t *rdma_buff;
-    size_t remote_segment_size = 0;
+    volatile rdma_buff_t *rdma_buff;
     sci_remote_segment_t remote_segment = NULL;
+    sci_sequence_t remote_sequence;
 
-    remote_connect_init(v_dev, &remote_segment, remote_segment_size, receiver_id, error);
+    remote_connect_init(v_dev, &remote_segment, receiver_id);
 
-    rdma_buff = (rdma_buff_t*) SCIMapRemoteSegment(remote_segment,
-                                                  &remote_map,
-                                                  NO_OFFSET,
-                                                  RECEIVER_SEG_SIZE,
-                                                  NO_SUG_ADDR,
-                                                  NO_FLAGS,
-                                                  &error);
-    print_sisci_error(&error, "SCIMapRemoteSegment", true);
+    rma_init(remote_segment, (volatile void **) &rdma_buff, &remote_map);
+    fprintf(stderr, "Remote segment mapped\n");
+    rma_sequence_init(remote_map, &remote_sequence);
+    rma_check(remote_sequence);
 
     printf("Node waiting for transfer\n");
 
+    printf("Checking done\n");
     while (!rdma_buff->done);
-    printf("RDMA Done!\n");
-
-    SCIUnmapSegment(remote_map, NO_FLAGS, &error);
-    print_sisci_error(&error, "SCIUnmapSegment", false);
+    printf("RDMA Done! Word: %s\n", rdma_buff->word);
+    rma_destroy_sequence(remote_sequence);
+    rma_destroy(remote_map);
 }
 
 int main(int argc, char *argv[]) {
@@ -95,6 +90,7 @@ int main(int argc, char *argv[]) {
     mode = argv[argc-1];
     if (strcmp(mode, "poll") != 0 &&
         strcmp(mode, "rma") != 0) print_usage(argv[0]);
+    if (strcmp(mode, "rma") == 0 && receiver_id == UNINITIALIZED_ARG) print_usage(argv[0]);
 
     SCIInitialize(NO_FLAGS, &error);
     print_sisci_error(&error, "SCIInitialize", true);
@@ -109,7 +105,7 @@ int main(int argc, char *argv[]) {
     print_sisci_error(&error, "SCIOpen", true);
 
     if (strcmp(mode, "poll") == 0) {
-        poll(v_dev, local_node_id, receiver_id, channel_id);
+        poll(v_dev, local_node_id);
     } else if (strcmp(mode, "rma") == 0) {
         rma(v_dev, receiver_id);
     } else {
