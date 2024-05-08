8f2760666fe240f5679d219e7a50646001f1d459
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-05-08 17:42:52 +0200

    print node id tool script

---
+++ tools/print_node_id.c
@@ -0,0 +1,28 @@
+#include <sisci_api.h>
+#include <sisci_error.h>
+#include <sisci_types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "sisci_glob_defs.h"
+
+
+int main(void) {
+    sci_desc_t sd;
+
+    SEOE(SCIInitialize, NO_FLAGS);
+    SEOE(SCIOpen, &sd, NO_FLAGS);
+
+    unsigned int local_node_id;
+    sci_query_adapter_t query;
+
+    query.subcommand = SCI_Q_ADAPTER_NODEID;
+    query.localAdapterNo = ADAPTER_NO;
+    query.data = &local_node_id;
+
+    SEOE(SCIQuery, SCI_Q_ADAPTER, &query, NO_FLAGS);
+
+    printf("%d\n", local_node_id);
+
+    SEOE(SCIClose, sd, NO_FLAGS);
+    SCITerminate();
+}
