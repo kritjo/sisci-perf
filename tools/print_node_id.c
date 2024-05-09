afe4db548ea6e691b36bc300e8ccff06203ffccd
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-05-09 16:03:15 +0200

    Refactor get_node_id as library function

--- tools/print_node_id.c
+++ tools/print_node_id.c
@@ -1,24 +1,15 @@
 #include <sisci_api.h>
 #include <sisci_error.h>
-#include <sisci_types.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "print_node_id.h"
 #include "sisci_glob_defs.h"
 
 
 int main(void) {
-    unsigned int local_node_id;
-    sci_query_adapter_t query;
-
     SEOE(SCIInitialize, NO_FLAGS);
 
-    query.subcommand = SCI_Q_ADAPTER_NODEID;
-    query.localAdapterNo = ADAPTER_NO;
-    query.data = &local_node_id;
-
-    SEOE(SCIQuery, SCI_Q_ADAPTER, &query, NO_FLAGS);
-
-    printf("%d\n", local_node_id);
+    printf("%d\n", get_node_id());
 
     SCITerminate();
 }
