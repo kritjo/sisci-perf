880b802b006fffdf35a4adf84345e58b9edb1432
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-05-10 17:29:28 +0200

    Actually connect to all of the segments

--- initiator/scale_up_pio.c
+++ initiator/scale_up_pio.c
@@ -42,68 +42,71 @@ void run_scale_up_segment_experiment_pio(sci_desc_t sd, pid_t main_pid, sci_remo
     unsigned int size;
     volatile char *data[MAX_SEGMENTS];
     sci_error_t error;
-    unsigned int segment_number;
 
     init_timer(MEASURE_SECONDS);
     timer_expired = get_timer_expired();
 
-    for (uint32_t i = 0; i < MAX_SEGMENTS; i++) {
-        order.commandType = COMMAND_TYPE_CREATE;
-        order.orderType = ORDER_TYPE_SEGMENT;
-        order.size = SEGMENT_SIZE;
+    for (uint32_t segments_this_round = 1; segments_this_round <= MAX_SEGMENTS; segments_this_round++) {
 
-        SEOE(SCITriggerDataInterrupt, order_interrupt, &order, sizeof(order), NO_FLAGS);
+        for (uint32_t i = 0; i < segments_this_round; i++) {
+            order.commandType = COMMAND_TYPE_CREATE;
+            order.orderType = ORDER_TYPE_SEGMENT;
+            order.size = SEGMENT_SIZE;
 
-        size = sizeof(delivery);
+            SEOE(SCITriggerDataInterrupt, order_interrupt, &order, sizeof(order), NO_FLAGS);
 
-        SEOE(SCIWaitForDataInterrupt, delivery_interrupt, &delivery, &size, SCI_INFINITE_TIMEOUT, NO_FLAGS);
+            size = sizeof(delivery);
 
-        if (delivery.commandType != COMMAND_TYPE_CREATE || delivery.deliveryType != ORDER_TYPE_SEGMENT ||
-            delivery.status != STATUS_TYPE_SUCCESS) {
-            fprintf(stderr, "Received invalid command type %d\n", delivery.commandType);
-            kill(main_pid, SIGTERM);
+            SEOE(SCIWaitForDataInterrupt, delivery_interrupt, &delivery, &size, SCI_INFINITE_TIMEOUT, NO_FLAGS);
+
+            if (delivery.commandType != COMMAND_TYPE_CREATE || delivery.deliveryType != ORDER_TYPE_SEGMENT ||
+                delivery.status != STATUS_TYPE_SUCCESS) {
+                fprintf(stderr, "Received invalid command type %d\n", delivery.commandType);
+                kill(main_pid, SIGTERM);
+            }
+
+            SEOE(SCIConnectSegment, sd, &segment[i], delivery.nodeId, delivery.id, ADAPTER_NO, NO_CALLBACK, NO_ARG,
+                 SCI_INFINITE_TIMEOUT, NO_FLAGS);
+            printf("Connected segment %d\n", delivery.id);
+
+            data[i] = SCIMapRemoteSegment(segment[i], &map[i], NO_OFFSET, SEGMENT_SIZE, NO_SUGGESTED_ADDRESS, NO_FLAGS,
+                                          &error);
+            if (error != SCI_ERR_OK) {
+                fprintf(stderr, "Failed to map segment: %d\n", error);
+                kill(main_pid, SIGTERM);
+            }
         }
 
-        SEOE(SCIConnectSegment, sd, &segment[i], delivery.nodeId, delivery.id, ADAPTER_NO, NO_CALLBACK, NO_ARG,
-             SCI_INFINITE_TIMEOUT, NO_FLAGS);
-
-        data[i] = SCIMapRemoteSegment(segment[i], &map[i], NO_OFFSET, SEGMENT_SIZE, NO_SUGGESTED_ADDRESS, NO_FLAGS,
-                                      &error);
-        if (error != SCI_ERR_OK) {
-            fprintf(stderr, "Failed to map segment: %d\n", error);
-            kill(main_pid, SIGTERM);
-        }
-
-        segment_number = i + 1;
-        printf("Starting PIO write for %d seconds with %d segments on same peer\n", MEASURE_SECONDS, segment_number);
+        printf("Starting PIO write for %d seconds with %d segments on same peer\n", MEASURE_SECONDS, segments_this_round);
         operations = 0;
         start_timer();
-        write_pio(data, segment_number);
+        write_pio(data, segments_this_round);
         printf("    operations: %llu\n", operations);
 
-        printf("Starting PIO read for %d seconds with %d segments on same peer\n", MEASURE_SECONDS, segment_number);
+        printf("Starting PIO read for %d seconds with %d segments on same peer\n", MEASURE_SECONDS, segments_this_round);
         operations = 0;
         start_timer();
-        read_pio(data, segment_number, main_pid);
+        read_pio(data, segments_this_round, main_pid);
         printf("    operations: %llu\n", operations);
 
-        SEOE(SCIUnmapSegment, map[i], NO_FLAGS);
+        for (uint32_t i = 0; i < segments_this_round; i++) {
+            SEOE(SCIUnmapSegment, map[i], NO_FLAGS);
 
-        SEOE(SCIDisconnectSegment, segment[i], NO_FLAGS);
+            SEOE(SCIDisconnectSegment, segment[i], NO_FLAGS);
 
+            order.commandType = COMMAND_TYPE_DESTROY;
 
-        order.commandType = COMMAND_TYPE_DESTROY;
+            SEOE(SCITriggerDataInterrupt, order_interrupt, &order, sizeof(order), NO_FLAGS);
 
-        SEOE(SCITriggerDataInterrupt, order_interrupt, &order, sizeof(order), NO_FLAGS);
+            size = sizeof(delivery);
 
-        size = sizeof(delivery);
+            SEOE(SCIWaitForDataInterrupt, delivery_interrupt, &delivery, &size, SCI_INFINITE_TIMEOUT, NO_FLAGS);
 
-        SEOE(SCIWaitForDataInterrupt, delivery_interrupt, &delivery, &size, SCI_INFINITE_TIMEOUT, NO_FLAGS);
-
-        if (delivery.commandType != COMMAND_TYPE_DESTROY || delivery.deliveryType != ORDER_TYPE_SEGMENT ||
-            delivery.status != STATUS_TYPE_SUCCESS) {
-            fprintf(stderr, "Received invalid command type %d\n", delivery.commandType);
-            kill(main_pid, SIGTERM);
+            if (delivery.commandType != COMMAND_TYPE_DESTROY || delivery.deliveryType != ORDER_TYPE_SEGMENT ||
+                delivery.status != STATUS_TYPE_SUCCESS) {
+                fprintf(stderr, "Received invalid command type %d\n", delivery.commandType);
+                kill(main_pid, SIGTERM);
+            }
         }
     }
 
