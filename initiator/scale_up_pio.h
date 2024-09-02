a555f6cc506d8092f9994461cf8a662aa359310d
Kristian Tjelta Johansen <kritjo@kritjo.com>
2024-09-02 17:06:04 +0200

    Scale up to 8 segments

--- initiator/scale_up_pio.h
+++ initiator/scale_up_pio.h
@@ -5,7 +5,7 @@
 #include <sys/types.h>
 
 #define MEASURE_SECONDS 2
-#define MAX_SEGMENTS 4
+#define MAX_SEGMENTS 8
 
 void run_scale_up_segment_experiment_pio(sci_desc_t sd, pid_t main_pid, sci_remote_data_interrupt_t order_interrupt, sci_local_data_interrupt_t delivery_interrupt);
 
