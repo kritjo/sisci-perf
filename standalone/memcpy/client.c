1a39b9dac62aacbf0c75a7d37e634d22e3b52ebb
Kristian Tjelta Johansen <kristianj@dolphinics.com>
2025-10-20 17:22:27 +0200

    Update usage message in client.c for clarity

--- standalone/memcpy/client.c
+++ standalone/memcpy/client.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <remote node id> <size of 'read'>\n", argv[0]);
+        printf("Usage: %s <remote node id> <size>\n", argv[0]);
         exit(1);
     }
 
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     int size = atoi(argv[2]);
 
     printf("Remote node id: %d\n", remote_node_id);
-    printf("Size of 'read': %d\n", size);
+    printf("Size: %d\n", size);
 
     sci_desc_t sd;
     sci_error_t error;
